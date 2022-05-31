5d70b9be19d3c7a596855130ded0f69a97a599f2
admitriy1931 <82140063+admitriy1931@users.noreply.github.com>
2022-06-01 01:15:51 +0500

    .

--- Task2/src/Graph.java
+++ Task2/src/Graph.java
@@ -1,13 +1,13 @@
 import java.util.*;
 
 class Graph {
-    public int[] arr = new int[6];
+    public int[] arr;
     public boolean cycl = false;
     private LinkedList<Vertice> adjLists[];
 
     Graph(int verticesCount) {
         adjLists = new LinkedList[verticesCount];
-
+        arr = new int[verticesCount];
         for (int i = 0; i < verticesCount; i++)
             adjLists[i] = new LinkedList();
     }
